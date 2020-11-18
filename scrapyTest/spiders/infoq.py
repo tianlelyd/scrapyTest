c3c5bac1eb50579aad0311e254cabde8af3a5e02
Liyd <tianlelyd@gmail.com>
2020-11-19 00:20:27 +0800

    测试用xpath进行获取

--- scrapyTest/spiders/infoq.py
+++ scrapyTest/spiders/infoq.py
@@ -4,7 +4,8 @@ import scrapy
 class InfoqSpider(scrapy.Spider):
     name = 'infoq'
     allowed_domains = ['www.infoq.cn']
-    start_urls = ['http://www.infoq.cn/']
+    start_urls = ['https://www.infoq.cn/theme/85']
 
     def parse(self, response):
+        re_selector = response.xpath('/html/head/title/text()')
         pass
