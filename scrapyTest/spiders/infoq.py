99c38343254f09c4e060fd4237592712db593acf
Liyd <tianlelyd@163.com>
2020-11-19 17:59:29 +0800

    增加css匹配

--- scrapyTest/spiders/infoq.py
+++ scrapyTest/spiders/infoq.py
@@ -4,8 +4,11 @@ import scrapy
 class InfoqSpider(scrapy.Spider):
     name = 'infoq'
     allowed_domains = ['www.infoq.cn']
-    start_urls = ['https://www.infoq.cn/theme/85']
+    start_urls = ['https://www.infoq.cn/article/DBSU7KiLwhyR8rUpBIT9']
 
     def parse(self, response):
+        # xpath 方式匹配
         re_selector = response.xpath('/html/head/title/text()')
+        # css 方式匹配
+        re_selector_css = response.css('title::text').extract_first()
         pass
