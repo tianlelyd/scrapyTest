9dec37ac97f21b6bfe7f6ab5b1693de5c5fbef28
Liyd <tianlelyd@gmail.com>
2020-11-26 00:18:55 +0800

    爬取列表页URL

--- scrapyTest/spiders/infoq.py
+++ scrapyTest/spiders/infoq.py
@@ -1,12 +1,23 @@
 import scrapy
-
+from scrapy.http import Request
+from urllib import parse
 
 class InfoqSpider(scrapy.Spider):
     name = 'infoq'
     allowed_domains = ['www.infoq.cn']
-    start_urls = ['https://www.infoq.cn/article/DBSU7KiLwhyR8rUpBIT9']
+    start_urls = ['https://readhub.cn/topics']
+    base_url = 'https://readhub.cn'
 
     def parse(self, response):
+        # 解析列表中的所有文章url并交给scrapy下载后并进行解析
+        post_urls = response.css('.topicItem___1B0j1 .topicTitle___1HWIA a::attr(href)').extract()
+        for post_url in post_urls:
+            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail)
+
+        # 提取下一页并交给scrapy下载
+
+    def parse_detail(self, response):
+        # 提取文章的具体字段
         # xpath 方式匹配
         re_selector = response.xpath('/html/head/title/text()')
         # css 方式匹配
